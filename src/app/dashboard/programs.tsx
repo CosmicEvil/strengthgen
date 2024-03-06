539cfbc6fd297b69d6cb9e9f0875a9bbe299a4cc
Wouter Massie <wouter.massie@acolad.com>
2024-03-06 23:21:11 +0100

    icon

--- src/app/dashboard/programs.tsx
+++ src/app/dashboard/programs.tsx
@@ -83,12 +83,16 @@ export function ProgramsHomepage(props: {
               </CardContent>
               <CardFooter className="flex pb-0 gap-6 items-end justify-between">
               
-                <Button onClick={(e) => deleteProgram(item._id)} className="bg-red-500 hover:bg-red-300 text-white">Delete</Button>
+                <Button onClick={(e) => deleteProgram(item._id)} className="bg-red-700 hover:bg-red-300 text-white shadow-md hover:shadow-lg shadow-red-500/90">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='w-4 fill-white'>
+                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                    </svg>
+                  </Button>
                 <Link
                   href={`/program/${item._id}`}
                   key={index}
                 >
-                  <Button  className="bg-cyan-500 hover:bg-cyan-300 text-white">See full Workout</Button>
+                  <Button  className="bg-cyan-500 hover:bg-cyan-300 text-white shadow-lg shadow-cyan-500/90">See full Workout</Button>
                 </Link>
               </CardFooter>
             </Card>
